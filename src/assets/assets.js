7bfc7c54f9860bf7351a4005a639f6309fedee6e
adeshdatir99ad <63045592+adeshdatir99ad@users.noreply.github.com>
2024-04-28 20:13:04 +0530

    img file change

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,6 @@
 import basket_icon from './basket_icon.png'
 import logo from './logo.png'
-import header_img from './header_img.png'
+import header_img from './header_img.jpg'
 import search_icon from './search_icon.png'
 import menu_1 from './menu_1.png'
 import menu_2 from './menu_2.png'
